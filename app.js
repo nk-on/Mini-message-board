6a5a62179a02d349649de9f1b1cb177db282c144
nk-on <2nkoz@proton.me>
2025-09-06 23:01:47 +0400

    Moving mock database to separate file

--- app.js
+++ app.js
@@ -2,20 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const port = process.env.PORT || 8000;
+const messages = require('./db')
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   return res.render("index", { title: "messages", messages });
